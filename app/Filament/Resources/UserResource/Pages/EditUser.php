aed83d12a099dd6324b0a91bccd759c84be0cae6
makkowens24 <exerateanalytical@gmail.com>
2026-06-13 20:52:00 +0100

    fix: guard self-deletion on user edit page header action

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -13,7 +13,8 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->hidden(fn () => $this->record->id === auth()->id()),
         ];
     }
 
